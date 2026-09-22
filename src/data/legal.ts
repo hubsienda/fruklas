1f282304abb9891e5d4f4bc5e54507b5ee939ac4
hubsienda <hub@siendaweblines.com>
2026-09-22 18:45:50 +0200

    Restore external FRUKLAS Privacy resource

--- src/data/legal.ts
+++ src/data/legal.ts
@@ -1,11 +1,4 @@
 export const legalResources = {
   dataUseNoticeUrl: null as string | null,
-  privacyPolicyUrl: '/es/legal/privacidad/',
-  officialSources: {
-    avisoLegal: 'https://legal.fruklas.com/aviso-legal',
-    privacidad: 'https://legal.fruklas.com/privacidad',
-    accessibilidad: 'https://legal.fruklas.com/accessibilidad',
-    igualdad: 'https://drive.google.com/file/d/1wJT85RCcaToCLrwOoZG_DRkbZMcKRMW_/view',
-    canalEtico: 'https://drive.google.com/file/d/1YRXXtwrY9mv2k8e-CHvRk1WtExqnHXpw/view',
-  },
+  privacyPolicyUrl: 'https://legal.fruklas.com/privacidad',
 } as const;
